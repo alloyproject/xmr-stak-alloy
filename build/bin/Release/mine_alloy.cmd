24b03f3bc1adc3af8f80fc345cdd2fb3da2289b5
nighthawk <nighthawk18@protonmail.com>
2018-06-23 10:51:21 -0700

    first commit

---
+++ build/bin/Release/mine_alloy.cmd
@@ -0,0 +1,2 @@
+del *.txt
+xmr-stak  --currency cryptonight_alloy -o stratum+tcp://alloypool.com:3333 -u A7cugshpEFRh8rubiTgiWrEJtJ5HbWRZZSXBZiqjbgvjKMZshSARNosJ5c9UA4fbQCVeUo51T2FEy69itj4T1mHUQBbDL7f -p x
---
+++ winbuild.cmd
@@ -0,0 +1,5 @@
+set CMAKE_PREFIX_PATH=C:\xmr-stak-dep\hwloc
+cmake.exe -G  "Visual Studio 14 2015 Win64" -T v140,host=x64 -DMICROHTTPD_ENABLE=OFF -DOpenSSL_ENABLE=OFF  -DOpenCL_ENABLE=ON  -DCMAKE_LINK_STATIC=ON .. 
+cmake --build . --config Release --target install
+
+
